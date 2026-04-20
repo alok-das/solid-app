e89d5c677482ae1ee1bf9e5f2a370768a953e345
ITADMIN <itadmin@ITAdmin.local>
2026-04-20 18:08:38 +0530

    Initial commit: Setup SolidJS app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { createSignal } from 'solid-js';
+import styles from './App.module.css';
+
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+        <p class={styles.counter}>
+          <button onClick={() => setCount(count() - 1)}>-</button>
+          <output>{count()}</output>
+          <button onClick={() => setCount(count() + 1)}>+</button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
